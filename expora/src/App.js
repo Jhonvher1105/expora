4a03e7199f02fe6f2b4b0767fdb9e073b5354bbf
Jhonvher1105 <126886509+Jhonvher1105@users.noreply.github.com>
2025-10-03 22:35:48 +0800

    create log in

--- expora/src/App.js
+++ expora/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import header from './components/header.js';
+import logIn from "./components/logIn.js";
 
 function App() {
   return (
     <div className="App">
-      {header()}
+      {/* {header()} */}
+      {logIn()}
     </div>
   );
 }
--- expora/src/components/logIn.js
+++ expora/src/components/logIn.js
@@ -1,7 +1,33 @@
-function logIn(){
-    return 
-    <div>
-        
+import logo from "../pic/logo.png";
+
+function logIn() {
+    return <div id="logIn-body">
+        <div>
+            <main id="login-main">
+                <div id="div-logIn-logo">
+                    <img src={logo} alt="expora logo" id="img-login-logo"></img>
+                    <h2>Expora</h2>
+                </div>
+                <div id="div-form-container">
+                    <h2>Welcome back</h2>
+                    <p>Sign in to your Expora account</p>
+                    <div id="#div-form">
+                        <form>
+                            <labe>Email address</labe>
+                            <input type="text" id="email" name="email" placeholder="Enter your email" required></input>
+
+                            <label for="password">Password</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password" required></input>
+                        </form>
+                        <div id="div-forget-Pass">
+                            <p><a href="/">Forgot your password?</a></p>
+                            <p>Don't have an account? <a href="/">Sign up</a></p>
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 }
 export default logIn;
